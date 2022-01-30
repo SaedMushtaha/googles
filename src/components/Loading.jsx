14c457d5ef67e0f97f39833a111fbfc6e6f0a18a
Saed Mushtaha <saed@mostidea.com.tr>
2022-01-30 18:05:52 +0300

    googles project - search engine

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// import Loader from 'react-loader-spinner';
+
+export const Loading = () => (
+  <div className="flex justify-center items-center ">
+      loading...
+    {/* <Loader type="Puff" color="#00BFFF" height={550} width={80} /> */}
+  </div>
+);
---
+++ src/contexts/StateContextProvider.js
@@ -0,0 +1,35 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const StateContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getResults = async (url) => {
+    setLoading(true);
+
+    const res = await fetch(`${baseUrl}${url}`, {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+        'x-rapidapi-key': '22e6efef80msh70156ef75103547p1db60djsnad2eb1ebe17c',  //process.env.REACT_APP_API_KEY
+      },
+    });
+
+    const data = await res.json();
+
+    setResults(data);
+    setLoading(false);
+  };
+
+  return (
+    <StateContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, loading }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
